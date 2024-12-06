9c8ddff22237197622a88a4e3f992fadd8324f99
Tilakna Gunawardhane <140165443+tilaknagunawardhane@users.noreply.github.com>
2024-12-06 23:18:05 +0530

    Sign In page

---
+++ components/FormField.jsx
@@ -0,0 +1,51 @@
+import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
+import React, { useState } from "react";
+import { icons } from "../constants";
+
+const FormField = ({
+  title,
+  value,
+  placeholder,
+  handleChangeText,
+  otherStyles,
+  ...props
+}) => {
+  const [isFocused, setIsFocused] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <View className={`space-y-2 ${otherStyles}`}>
+      <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
+
+      <View
+        className={`w-full h-16 px-4 bg-black-100 rounded-2xl border-2 flex flex-row items-center ${
+          isFocused ? "border-secondary" : "border-gray-800"
+        }`}
+      >
+        <TextInput
+          className="flex-1 text-white font-psemibold text-base"
+          value={value}
+          placeholder={placeholder}
+          placeholderTextColor="#7B7B8B"
+          onChangeText={handleChangeText}
+          secureTextEntry={title === "Password" && !showPassword}
+          onFocus={() => setIsFocused(true)} // Handle focus
+          onBlur={() => setIsFocused(false)} // Handle blur
+          {...props}
+        />
+
+        {title === "Password" && (
+          <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+            <Image
+              source={!showPassword ? icons.eye : icons.eyeHide}
+              className="w-6 h-6"
+              resizeMode="contain"
+            />
+          </TouchableOpacity>
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default FormField;
