35a8b52ed87841d270260bfee5751d7d6338039d
Tilakna Gunawardhane <140165443+tilaknagunawardhane@users.noreply.github.com>
2024-12-06 19:01:55 +0530

    Main Auth layout

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,12 +1,28 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 const AuthLayout = () => {
   return (
-    <View>
-      <Text>AuthLayout</Text>
-    </View>
-  )
-}
+    <>
+      <Stack
+        screenOptions={{
+          headerShown: false, // Hide headers for all screens in this stack
+        }}
+      >
+        <Stack.Screen name="sign_in" options={{ headerShown: false }} />
 
-export default AuthLayout
+        <Stack.Screen
+          name="sign_up"
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack>
+      <StatusBar backgroundColor="#161622" style="light" />
+    </>
+  );
+};
+
+export default AuthLayout;
