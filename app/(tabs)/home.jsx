1aaf8f20dc5b08daf305a67ae47a8d4fadc8427d
Tilakna Gunawardhane <140165443+tilaknagunawardhane@users.noreply.github.com>
2024-12-16 05:25:27 +0530

    Home page Frontend finished temporary

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,5 +1,5 @@
-import { View, Text, FlatList, Image } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList, Image, RefreshControl } from 'react-native'
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '../../constants'
 import SearchInput from '../../components/SearchInput'
@@ -7,11 +7,20 @@ import Trending from '../../components/Trending'
 import EmptyState from '../../components/EmptyState'
 
 const Home = () => {
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    // await refetch();
+    setRefreshing(false);
+  };
+
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <FlatList  
-        // data={[{id: 1}, {id: 2}, {id: 3}]}
-        data={[]}
+        data={[{id: 1}, {id: 2}, {id: 3}]}
+        // data={[]}
         keyExtractor={(item) => item.$id}
         renderItem={({item}) => (
           <Text className="text-3xl text-white">{item.id}</Text>
@@ -46,12 +55,17 @@ const Home = () => {
             </View>
           </View>
         )}
+
         ListEmptyComponent={() => (
           <EmptyState
             title="No Videos Found"
             subtitle="No videos created yet"
           />
         )}
+
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
       />
     </SafeAreaView>
   )
