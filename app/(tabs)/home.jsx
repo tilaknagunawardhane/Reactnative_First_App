3ecd4b483401ab9083025b6b8e7a3db0e87cd392
Tilakna Gunawardhane <140165443+tilaknagunawardhane@users.noreply.github.com>
2024-12-16 04:28:43 +0530

    Update home
    added search input

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,6 +1,8 @@
-import { View, Text, FlatList } from 'react-native'
+import { View, Text, FlatList, Image } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { images } from '../../constants'
+import SearchInput from '../../components/SearchInput'
 
 const Home = () => {
   return (
@@ -15,10 +17,27 @@ const Home = () => {
         ListHeaderComponent={() => (
           <View className="my-6 px-4 space-y-6">
             <View className=" justify-between items-start flex-row mb-6">
+              
               <View>
                 <Text className="font-pmedium text-sm text-gray-100">Welcome Back</Text>
                 <Text className="text-2xl font-psemibold text-white">JSMastery</Text>
               </View>
+
+              <View className="mt-1.5">
+                <Image
+                  source={images.logoSmall}
+                  className="w-9 h-10"
+                  resizeMode='contain'
+                />
+              </View>
+            </View>
+            <SearchInput/>
+
+            {/* Latest Videos Section */}
+            <View className="w-full flex-1 pt-5 pb-8">
+              <Text className="text-lg font-pregular text-gray-100 mb-3">
+                Latest Videos
+              </Text>
             </View>
           </View>
         )}
